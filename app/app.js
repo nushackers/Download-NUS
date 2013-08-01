5668090a676ddfb19d7ec50acc3fc26a141ed959
Fazli Sapuan <fazli@sapuan.org>
2013-08-01 20:17:07 +0800

    various fixes

--- app/app.js
+++ app/app.js
@@ -75,6 +75,7 @@ app.configure(function() {
     // set up routes
     app.use(express.methodOverride());
     app.use(app.router);
+    app.set('views', __dirname + '/../views');
     app.engine('html', ejs.renderFile);
     app.use(express.static(__dirname + '/../static'));
     app.use(function(err, req, res, next) {
