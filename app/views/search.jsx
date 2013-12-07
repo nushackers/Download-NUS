65bc9cd0d0add6979ba328d694ab01b55e647e84
yyjhao <yyjhao@gmail.com>
2013-12-07 13:36:44 +0800

    search error handling

--- app/views/search.jsx
+++ app/views/search.jsx
@@ -12,10 +12,11 @@ module.exports = React.createClass({
         <div>
             <searchform q={this.props.data.q} />
             {this.props.data.error ?
-                [
-                <h2>{this.props.data.error.title}</h2>,
-                <p>{this.props.data.error.description}</p>
-                ] :
+                <div className="alert alert-danger">
+                    <h2>{this.props.data.error.title}</h2>
+                    <p>{this.props.data.error.description}</p>
+                </div>
+                :
                 [
                 <h2>{"Search results for " + this.props.data.q}</h2>,
                 <datasets datasets={this.props.data.datasets} pages={this.props.data.pages} page={this.props.data.page}/>
