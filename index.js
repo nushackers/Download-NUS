45c4831a6916a0c85a6bee7fead67fbfea03fd61
yyjhao <yyjhao@gmail.com>
2013-12-13 11:38:59 +0800

    redis for session store

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var express = require('express'),
     app = express(),
     port = config.app.port,
     Router = require('./lib/ServerRouter'),
-    ApiClient = require('./lib/server_api_client');
+    ApiClient = require('./lib/server_api_client'),
+    connect = require("connect"),
+    RedisStore = require("connect-redis")(connect);
 
 // Allow directly requiring '.jsx' files.
 require('node-jsx').install({extension: '.jsx'});
+
+var sessionStore = new RedisStore(config.redisStore.option);
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/static'), { maxAge: config.app.cacheDuration });
@@ -15,7 +19,7 @@ app.use(express.static(__dirname + '/static'), { maxAge: config.app.cacheDuratio
 app.use(express.bodyParser());
 
 app.use(express.cookieParser(config.session.secret));
-app.use(express.session({ secret:config.session.secret }));
+app.use(express.session({ secret:config.session.secret, store: sessionStore }));
 var api = require('./lib/api')(app, "/api", config),
     apiClient = ApiClient(api),
     routes = require('./app/routes')(apiClient, config.app.insecure),
