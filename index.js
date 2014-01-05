79209b3f8bbac0edb88663bc84c57eee86d9abcd
yyjhao <yyjhao@gmail.com>
2014-01-05 16:42:51 +0800

    better upload security

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ var sessionStore = new RedisStore(_.extend({ client: redis },
 app.use(express.compress());
 app.use(express.static(__dirname + '/static'), { maxAge: config.app.cacheDuration });
 
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use(express.cookieParser(config.session.secret));
 app.use(express.session({ secret:config.session.secret, store: sessionStore }));
-var api = require('./lib/api')(app, "/api", config, redis),
+var api = require('./lib/api')(express, app, "/api", config, redis),
     apiClient = ApiClient(api),
     routes = require('./app/routes')(apiClient, config.app.insecure),
     router = new Router(routes);
