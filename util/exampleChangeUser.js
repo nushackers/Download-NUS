9ebed8c3a13b621e3b813f31474c52af6fd62e83
yyjhao <yyjhao@gmail.com>
2014-05-11 22:08:34 +0800

    user management script

---
+++ util/exampleChangeUser.js
@@ -0,0 +1,5 @@
+var manage = require("./userManagement.js");
+
+manage.makeSuper("a0099621");
+manage.invalidateSessions();
+console.log("wait for a while then ctrl-C (yes it's dumb but good enough for this kind of use)");
