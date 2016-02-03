5d66ee0a615227a359975d1ceac2c3c65dceb80f
jellyjellyrobot <jellyjellyrobot@gmail.com>
2016-02-03 18:36:24 +0800

    removed legacy studd

--- dan/util/exampleChangeUser.js
+++
@@ -1,5 +0,0 @@
-var manage = require("./userManagement.js");
-
-manage.makeSuper("a0099621");
-manage.invalidateSessions();
-console.log("wait for a while then ctrl-C (yes it's dumb but good enough for this kind of use)");
