a432546a79c9daee55c8a5a6accce2a6e96fea28
Jeremias Wong <jellyjellyrobot@gmail.com>
2016-03-06 15:46:48 +0800

    removed repos with low usage ratings

--- mirror-analysis/test.sh
+++ mirror-analysis/test.sh
@@ -3,5 +3,5 @@ FILES=/var/www/download/data/*
 for f in $FILES
 do
 	#cat /var/log/nginx/mirror.lo* | grep "/mirror/$(basename "$f")/" | grep -v "68.180.230.246" | wc -l
-	echo "$(basename "$f") | $(cat /var/log/nginx/mirror.lo* | grep "/mirror/$(basename "$f")/" | grep -v "68.180.230.246" | wc -l)"
+	echo "$(basename "$f") | $(cat /var/log/nginx/mirror.lo* | grep "/mirror/$(basename "$f")/" | grep -v "68.180.230.246" | grep -v "Baiduspider/2.0" | grep -v "http://www.mfisoft.ru/analyst/" | grep -v "http://ahrefs.com/robot/" | wc -l)"
 done
